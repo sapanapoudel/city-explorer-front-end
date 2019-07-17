5d0f22b68449b17e5ba25acaef13390e76b20747
sapanapoudel <sapana.poudel17@outlook.com>
2019-07-17 10:50:53 -0700

    component

---
+++ city-explorer/src/component/Search.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Search extends React.Component {
+  render() {
+    return (
+      <React.Fragment>
+        <form>
+        Search for the location <input></input>
+          <button>Explore!</button>
+        </form>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Search;
